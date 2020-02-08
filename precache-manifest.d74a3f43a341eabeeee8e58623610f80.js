9b5abea572f2b5688597f23f53170f949734bb77
tomomii0806 <dlcms4423@gmail.com>
2020-02-07 23:01:02 -0800

    Updates

---
+++ precache-manifest.d74a3f43a341eabeeee8e58623610f80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ddebb53517bc41ff7e5ac2b807cd869",
+    "url": "/hello-react/index.html"
+  },
+  {
+    "revision": "4cc8d1012429c2d5a9db",
+    "url": "/hello-react/static/css/main.5e861914.chunk.css"
+  },
+  {
+    "revision": "00a164b505c1734aa846",
+    "url": "/hello-react/static/js/2.368a41ff.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hello-react/static/js/2.368a41ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc8d1012429c2d5a9db",
+    "url": "/hello-react/static/js/main.b512170a.chunk.js"
+  },
+  {
+    "revision": "f0d834b638037e9fea77",
+    "url": "/hello-react/static/js/runtime-main.a37268f6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
